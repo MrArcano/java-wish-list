5cce02272f2c8ee66aabc577be958a3782a19812
MrArcano <salvatore-dario@hotmail.it>
2024-04-05 15:49:20 +0200

    [add] Read+Write list to file

--- src/org/experis/whisList/WishList.java
+++ src/org/experis/whisList/WishList.java
@@ -1,5 +1,9 @@
 package org.experis.whisList;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +12,8 @@ import java.util.Scanner;
 public class WishList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Gift> wishlist = new ArrayList<>();
+        // Creo una lista di Gift e gli inserisco una lista creata con il medoto static readFromFile()
+        List<Gift> wishlist = readFromFile();
 
         boolean continueAdding = true;
             while(continueAdding) {
@@ -27,11 +32,43 @@ public class WishList {
         // Sort the list
         Collections.sort(wishlist);
 
+        // Save list
+        saveToFile(wishlist);
+
         // Print the sorted list
         System.out.println("\nSorted list of gifts:");
         for(Gift gift : wishlist) {
             System.out.println(gift.getName());
         }
+    }
+
+    private static List<Gift> readFromFile() {
+
+        List<Gift> wishlist = new ArrayList<>();
+
+        try(Scanner fileReader = new Scanner(new File("./resources/data.txt"))) {
+
+            while (fileReader.hasNextLine()) {
+                wishlist.add(new Gift(fileReader.nextLine()));
+            }
+            System.out.println("Wishlist loaded from file.");
+        } catch (IOException e) {
+            System.out.println("Unable to open file");
+        }
+        return wishlist;
+    }
+
+    private static void saveToFile(List<Gift> wishlist) {
+
+        try(FileWriter fileWriter = new FileWriter(new File("./resources/data.txt"))){
+            for (Gift gift : wishlist) {
+                fileWriter.write(gift.getName() + "\n"); // Write each gift name followed by a newline character
+            }
+            System.out.println("Wishlist saved to file.");
+        } catch (IOException e) {
+            System.out.println("Unable to open file");
+        }
+
     }
 }
 
