17c28615450a9066e2ddf515254fa9afc7907bc7
MrArcano <salvatore-dario@hotmail.it>
2024-04-05 15:21:25 +0200

    [add] Gift class

---
+++ src/org/experis/whisList/Gift.java
@@ -0,0 +1,13 @@
+package org.experis.whisList;
+
+public class Gift {
+    private String name;
+
+    public Gift(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
